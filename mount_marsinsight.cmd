a2758c15cf0866a7610e83ccb60623659a3ab351
westernmarslab <51982574+westernmarslab@users.noreply.github.com>
2019-06-25 10:53:01 -0700

    Add files via upload

---
+++ mount_marsinsight.cmd
@@ -0,0 +1,2 @@
+netsh wlan connect ssid="Hoza House" name="Hoza House"
+net use v: \\MARSINSIGHT\SpecShare
---
+++ mount_pi.cmd
@@ -0,0 +1,3 @@
+netsh wlan connect ssid="Hoza House" name="Hoza House"
+ping 127.0.0.1 -n 6 > nul
+net use z: \\HOZAPI\PISHARE whiskey5 /user:HOZAPI\pi
